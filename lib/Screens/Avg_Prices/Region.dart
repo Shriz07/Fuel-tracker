bec9a2a84cac521e56f7cfce18e6b92746905d20
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-10 15:48:17 +0200

    Changed class name

---
+++ lib/Screens/Avg_Prices/Region.dart
@@ -0,0 +1,17 @@
+class Region {
+  Region({
+    required this.name,
+    required this.price95,
+    required this.price98,
+    required this.priceON,
+    required this.priceONplus,
+    required this.priceLPG,
+  });
+
+  String name;
+  String price95;
+  String price98;
+  String priceON;
+  String priceONplus;
+  String priceLPG;
+}
