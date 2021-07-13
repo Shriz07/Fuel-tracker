14feae6f9009ce58777e6ba280cf73ffbcda2ebf
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-13 19:48:00 +0200

    Code cleanup

--- lib/Screens/Avg_Prices/AvgPrices.dart
+++ lib/Screens/Avg_Prices/AvgPrices.dart
@@ -35,8 +35,7 @@ class _MyAppState extends State<AvgPrices> {
 
   Future<List> getPrices() async {
     if (regions.isEmpty) {
-      var response = await http.Client()
-          .get(Uri.parse('https://www.autocentrum.pl/paliwa/ceny-paliw/'));
+      var response = await http.Client().get(Uri.parse('https://www.autocentrum.pl/paliwa/ceny-paliw/'));
 
       var document = parse(response.body);
 
@@ -52,13 +51,7 @@ class _MyAppState extends State<AvgPrices> {
         var priceONplus = removeSingsFromPrice(tbody[i++].text);
         var priceLPG = removeSingsFromPrice(tbody[i++].text);
 
-        var petrol = Region(
-            name: name,
-            price95: price95,
-            price98: price98,
-            priceON: priceON,
-            priceONplus: priceONplus,
-            priceLPG: priceLPG);
+        var petrol = Region(name: name, price95: price95, price98: price98, priceON: priceON, priceONplus: priceONplus, priceLPG: priceLPG);
 
         regions.add(petrol);
       }
@@ -86,8 +79,7 @@ class _MyAppState extends State<AvgPrices> {
     return Padding(
       padding: EdgeInsets.only(top: 24.0),
       child: ConstrainedBox(
-        constraints:
-            BoxConstraints.expand(width: MediaQuery.of(context).size.width),
+        constraints: BoxConstraints.expand(width: MediaQuery.of(context).size.width),
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: DataTable(
@@ -95,169 +87,148 @@ class _MyAppState extends State<AvgPrices> {
             sortColumnIndex: _currentSortColumn,
             columnSpacing: 0,
             horizontalMargin: 15,
-            headingRowColor:
-                MaterialStateColor.resolveWith((states) => Colors.green),
+            headingRowColor: MaterialStateColor.resolveWith((states) => Colors.green),
             columns: <DataColumn>[
-              DataColumn(
-                  label: Text(
-                    'Województwo',
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                  onSort: (columnIndex, _) {
-                    setState(() {
-                      if (_isAscending == true) {
-                        _isAscending = false;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceB.name.compareTo(priceA.name));
-                      } else {
-                        _isAscending = true;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceA.name.compareTo(priceB.name));
-                      }
-                    });
-                  }),
-              DataColumn(
-                  label: Expanded(
-                      child: Image(
-                    image: AssetImage('assets/petrol95.png'),
-                  )),
-                  onSort: (columnIndex, _) {
-                    setState(() {
-                      if (_isAscending == true) {
-                        _isAscending = false;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceB.price95.compareTo(priceA.price95));
-                      } else {
-                        _isAscending = true;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceA.price95.compareTo(priceB.price95));
-                      }
-                    });
-                  }),
-              DataColumn(
-                  label: Expanded(
-                    child: Image(
-                      image: AssetImage('assets/petrol98.png'),
-                    ),
-                  ),
-                  onSort: (columnIndex, _) {
-                    setState(() {
-                      if (_isAscending == true) {
-                        _isAscending = false;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceB.price98.compareTo(priceA.price98));
-                      } else {
-                        _isAscending = true;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceA.price98.compareTo(priceB.price98));
-                      }
-                    });
-                  }),
-              DataColumn(
-                  label: Expanded(
-                    child: Image(
-                      image: AssetImage('assets/petrolON.png'),
-                    ),
-                  ),
-                  onSort: (columnIndex, _) {
-                    setState(() {
-                      if (_isAscending == true) {
-                        _isAscending = false;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceB.priceON.compareTo(priceA.priceON));
-                      } else {
-                        _isAscending = true;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceA.priceON.compareTo(priceB.priceON));
-                      }
-                    });
-                  }),
-              DataColumn(
-                  label: Expanded(
-                    child: Image(
-                      image: AssetImage('assets/petrolONplus.png'),
-                    ),
-                  ),
-                  onSort: (columnIndex, _) {
-                    setState(() {
-                      if (_isAscending == true) {
-                        _isAscending = false;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceB.priceONplus.compareTo(priceA.priceONplus));
-                      } else {
-                        _isAscending = true;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceA.priceONplus.compareTo(priceB.priceONplus));
-                      }
-                    });
-                  }),
-              DataColumn(
-                  label: Expanded(
-                    child: Image(
-                      image: AssetImage('assets/petrolLPG.png'),
-                    ),
-                  ),
-                  onSort: (columnIndex, _) {
-                    setState(() {
-                      if (_isAscending == true) {
-                        _isAscending = false;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceB.priceLPG.compareTo(priceA.priceLPG));
-                      } else {
-                        _isAscending = true;
-                        _currentSortColumn = columnIndex;
-                        regions.sort((priceA, priceB) =>
-                            priceA.priceLPG.compareTo(priceB.priceLPG));
-                      }
-                    });
-                  }),
+              displayRegionNameHeader(),
+              displayPetrolHeader('assets/petrol95.png', '95'),
+              displayPetrolHeader('assets/petrol98.png', '98'),
+              displayPetrolHeader('assets/petrolON.png', 'ON'),
+              displayPetrolHeader('assets/petrolONplus.png', 'ONplus'),
+              displayPetrolHeader('assets/petrolLPG.png', 'LPG'),
             ],
-            rows: regions
-                .map(
-                  ((element) => DataRow(
-                        color: MaterialStateColor.resolveWith((states) {
-                          return regions.indexOf(element) % 2 == 0
-                              ? Colors.amberAccent
-                              : Colors.lightGreen;
-                        }),
-                        cells: <DataCell>[
-                          DataCell(
-                            Text(element.name, textAlign: TextAlign.center),
-                          ),
-                          DataCell(
-                            Text(element.price95, textAlign: TextAlign.center),
-                          ),
-                          DataCell(
-                            Text(element.price98, textAlign: TextAlign.center),
-                          ),
-                          DataCell(
-                            Text(element.priceON, textAlign: TextAlign.center),
-                          ),
-                          DataCell(
-                            Text(element.priceONplus,
-                                textAlign: TextAlign.center),
-                          ),
-                          DataCell(
-                            Text(element.priceLPG, textAlign: TextAlign.center),
-                          ),
-                        ],
-                      )),
-                )
-                .toList(),
+            rows: getRowsWithPrices(),
           ),
         ),
       ),
     );
+  }
+
+  List<DataRow> getRowsWithPrices() {
+    return regions
+        .map(
+          ((element) => DataRow(
+                color: MaterialStateColor.resolveWith((states) {
+                  return regions.indexOf(element) % 2 == 0 ? Colors.amberAccent : Colors.lightGreen;
+                }),
+                cells: <DataCell>[
+                  DataCell(
+                    Text(
+                      element.name,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DataCell(
+                    Text(element.price95, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  DataCell(
+                    Text(element.price98, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  DataCell(
+                    Text(element.priceON, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  DataCell(
+                    Text(element.priceONplus, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  DataCell(
+                    Text(element.priceLPG, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              )),
+        )
+        .toList();
+  }
+
+  DataColumn displayRegionNameHeader() {
+    return DataColumn(
+        label: Text(
+          'Województwo',
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+        onSort: (columnIndex, _) {
+          setState(() {
+            if (_isAscending == true) {
+              _isAscending = false;
+              _currentSortColumn = columnIndex;
+              regions.sort((priceA, priceB) => priceB.name.compareTo(priceA.name));
+            } else {
+              _isAscending = true;
+              _currentSortColumn = columnIndex;
+              regions.sort((priceA, priceB) => priceA.name.compareTo(priceB.name));
+            }
+          });
+        });
+  }
+
+  DataColumn displayPetrolHeader(String assetImage, String fuelType) {
+    return DataColumn(
+        label: Expanded(
+          child: Image(
+            image: AssetImage(assetImage),
+          ),
+        ),
+        onSort: (columnIndex, _) {
+          if (fuelType == 'LPG') {
+            setState(() {
+              if (_isAscending == true) {
+                _isAscending = false;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceB.priceLPG.compareTo(priceA.priceLPG));
+              } else {
+                _isAscending = true;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceA.priceLPG.compareTo(priceB.priceLPG));
+              }
+            });
+          } else if (fuelType == '95') {
+            setState(() {
+              if (_isAscending == true) {
+                _isAscending = false;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceB.price95.compareTo(priceA.price95));
+              } else {
+                _isAscending = true;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceA.price95.compareTo(priceB.price95));
+              }
+            });
+          } else if (fuelType == '98') {
+            setState(() {
+              if (_isAscending == true) {
+                _isAscending = false;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceB.price98.compareTo(priceA.price98));
+              } else {
+                _isAscending = true;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceA.price98.compareTo(priceB.price98));
+              }
+            });
+          } else if (fuelType == 'ON') {
+            setState(() {
+              if (_isAscending == true) {
+                _isAscending = false;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceB.priceON.compareTo(priceA.priceON));
+              } else {
+                _isAscending = true;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceA.priceON.compareTo(priceB.priceON));
+              }
+            });
+          } else if (fuelType == 'ONplus') {
+            setState(() {
+              if (_isAscending == true) {
+                _isAscending = false;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceB.priceONplus.compareTo(priceA.priceONplus));
+              } else {
+                _isAscending = true;
+                _currentSortColumn = columnIndex;
+                regions.sort((priceA, priceB) => priceA.priceONplus.compareTo(priceB.priceONplus));
+              }
+            });
+          }
+        });
   }
 }
