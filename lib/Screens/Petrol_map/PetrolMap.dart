b3af02cd3a67119ed18fd25ce05363b30b17972e
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-13 19:22:49 +0200

    Added latest update information and code cleanup

--- lib/Screens/Petrol_map/PetrolMap.dart
+++ lib/Screens/Petrol_map/PetrolMap.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:fuel_tracker/Screens/Petrol_map/locations.dart';
@@ -7,6 +8,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'locations.dart' as locations;
+import 'package:time_ago_provider/time_ago_provider.dart' as timeAgo;
 
 class PetrolMap extends StatefulWidget {
   @override
@@ -124,256 +126,206 @@ class _MyAppState extends State<PetrolMap> {
   void _showDialog(Station station) async {
     await _db.getStation(station);
     await showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          return Dialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Container(
-              child: SafeArea(
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Form(
-                      key: _formkey,
-                      child: Column(
-                        children: <Widget>[
-                          Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: IconButton(
-                                  icon: Icon(
-                                    Icons.arrow_back_ios,
-                                    color: Colors.lightGreen,
-                                    size: 30,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.center,
-                                child: Title(
-                                  color: Colors.black,
-                                  child: Text(
-                                    station.name,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            station.vicinity,
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(height: 15),
-                          RatingBarIndicator(
-                            rating: station.rating,
-                            itemCount: 5,
-                            direction: Axis.horizontal,
-                            itemBuilder: (context, index) =>
-                                Icon(Icons.star, color: Colors.amber),
-                          ),
-                          SizedBox(height: 30),
-                          SizedBox(
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image(
-                                  image: AssetImage('assets/petrol95.png'),
-                                ),
-                                SizedBox(width: 15),
-                                Flexible(
-                                  child: TextFormField(
-                                    keyboardType: TextInputType.number,
-                                    controller: _petrol95Controller,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.amberAccent,
-                                      hintText: station.price95,
-                                      prefixIcon: Padding(
-                                          padding: const EdgeInsets.all(8),
-                                          child:
-                                              Image.asset('assets/drop.png')),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(15),
-                                        borderSide: const BorderSide(
-                                            color: Colors.lightGreen,
-                                            width: 3.0),
-                                      ),
-                                      border: const OutlineInputBorder(),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                          SizedBox(
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image(
-                                  image: AssetImage('assets/petrol98.png'),
-                                ),
-                                SizedBox(width: 15),
-                                Flexible(
-                                  child: TextFormField(
-                                    keyboardType: TextInputType.number,
-                                    controller: _petrol98Controller,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.amberAccent,
-                                      hintText: station.price98,
-                                      prefixIcon: Padding(
-                                          padding: const EdgeInsets.all(8),
-                                          child:
-                                              Image.asset('assets/drop.png')),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(15),
-                                        borderSide: const BorderSide(
-                                            color: Colors.lightGreen,
-                                            width: 3.0),
-                                      ),
-                                      border: const OutlineInputBorder(),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                          SizedBox(
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image(
-                                  image: AssetImage('assets/petrolON.png'),
-                                ),
-                                SizedBox(width: 15),
-                                Flexible(
-                                  child: TextFormField(
-                                    keyboardType: TextInputType.number,
-                                    controller: _petrolONController,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.amberAccent,
-                                      hintText: station.priceON,
-                                      prefixIcon: Padding(
-                                          padding: const EdgeInsets.all(8),
-                                          child:
-                                              Image.asset('assets/drop.png')),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(15),
-                                        borderSide: const BorderSide(
-                                            color: Colors.lightGreen,
-                                            width: 3.0),
-                                      ),
-                                      border: const OutlineInputBorder(),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                          SizedBox(
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image(
-                                  image: AssetImage('assets/petrolLPG.png'),
-                                ),
-                                SizedBox(width: 15),
-                                Flexible(
-                                  child: TextFormField(
-                                    keyboardType: TextInputType.number,
-                                    controller: _petrolLPGController,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.amberAccent,
-                                      hintText: station.priceLPG,
-                                      prefixIcon: Padding(
-                                          padding: const EdgeInsets.all(8),
-                                          child:
-                                              Image.asset('assets/drop.png')),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(15),
-                                        borderSide: const BorderSide(
-                                            color: Colors.lightGreen,
-                                            width: 3.0),
-                                      ),
-                                      border: const OutlineInputBorder(),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          Center(
-                            child: MaterialButton(
-                              onPressed: () {
-                                var dataChanged = false;
-                                if (_petrol95Controller.text != '') {
-                                  station.price95 = _petrol95Controller.text;
-                                  _petrol95Controller.text = '';
-                                  dataChanged = true;
-                                }
-                                if (_petrol98Controller.text != '') {
-                                  station.price98 = _petrol98Controller.text;
-                                  _petrol98Controller.text = '';
-                                  dataChanged = true;
-                                }
-                                if (_petrolONController.text != '') {
-                                  station.priceON = _petrolONController.text;
-                                  _petrolONController.text = '';
-                                  dataChanged = true;
-                                }
-                                if (_petrolLPGController.text != '') {
-                                  station.priceLPG = _petrolLPGController.text;
-                                  _petrolLPGController.text = '';
-                                  dataChanged = true;
-                                }
-                                if (dataChanged) {
-                                  _db.addStation(station);
-                                }
-                                Navigator.pop(context);
-                              },
-                              height: 50,
-                              color: Colors.lightGreen,
-                              textColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              child: Text(
-                                'Zapisz',
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                        ],
-                      ),
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Container(
+            child: SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Form(
+                    key: _formkey,
+                    child: Column(
+                      children: <Widget>[
+                        displayTitle(station.name, context),
+                        displayStationAddress(station.vicinity),
+                        SizedBox(height: 15),
+                        ratingBarIndicator(station.rating),
+                        SizedBox(height: 10),
+                        lastUpdateText(station),
+                        SizedBox(height: 10),
+                        petrolInputField('assets/petrol95.png', station.price95,
+                            _petrol95Controller),
+                        SizedBox(height: 15),
+                        petrolInputField('assets/petrol98.png', station.price98,
+                            _petrol98Controller),
+                        SizedBox(height: 15),
+                        petrolInputField('assets/petrolON.png', station.priceON,
+                            _petrolONController),
+                        SizedBox(height: 15),
+                        petrolInputField('assets/petrolLPG.png',
+                            station.priceLPG, _petrolLPGController),
+                        SizedBox(height: 30),
+                        saveButton(station, context),
+                      ],
                     ),
                   ),
                 ),
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
+  }
+
+  SizedBox petrolInputField(
+      String imageAsset, String price, TextEditingController textController) {
+    return SizedBox(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage(imageAsset),
+          ),
+          SizedBox(width: 15),
+          Flexible(
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              controller: textController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.amberAccent,
+                hintText: price,
+                prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Image.asset('assets/drop.png')),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide:
+                      const BorderSide(color: Colors.lightGreen, width: 3.0),
+                ),
+                border: const OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Stack displayTitle(String title, BuildContext currContext) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: goBackButton(currContext),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: displayStationName(title),
+        ),
+      ],
+    );
+  }
+
+  Title displayStationName(String name) {
+    return Title(
+      color: Colors.black,
+      child: Text(
+        name,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+    );
+  }
+
+  IconButton goBackButton(BuildContext currContext) {
+    return IconButton(
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: Colors.lightGreen,
+        size: 30,
+      ),
+      onPressed: () {
+        Navigator.pop(currContext);
+      },
+    );
+  }
+
+  Text displayStationAddress(String address) {
+    return Text(
+      address,
+      style: TextStyle(fontSize: 16),
+    );
+  }
+
+  RatingBarIndicator ratingBarIndicator(double rating) {
+    return RatingBarIndicator(
+      rating: rating,
+      itemCount: 5,
+      direction: Axis.horizontal,
+      itemBuilder: (context, index) => Icon(Icons.star, color: Colors.amber),
+    );
+  }
+
+  Center lastUpdateText(Station station) {
+    var dateTime1 = DateTime.now();
+    var dateTime2 = DateTime.fromMicrosecondsSinceEpoch(
+        station.updateTimestamp.microsecondsSinceEpoch);
+    var differenceBetweenDates = dateTime1.difference(dateTime2).inDays;
+    return Center(
+      child: differenceBetweenDates > 10000
+          ? Text('Ostatnia aktualizacja: Nigdy')
+          : Text(
+              'Ostatnia aktualizacja: ' +
+                  timeAgo.format(station.updateTimestamp.toDate(),
+                      locale: 'pl'),
+            ),
+    );
+  }
+
+  Center saveButton(Station station, BuildContext currContext) {
+    return Center(
+      child: MaterialButton(
+        onPressed: () {
+          updatePrices(station);
+          Navigator.pop(currContext);
+        },
+        height: 50,
+        color: Colors.lightGreen,
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Text(
+          'Zapisz',
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  void updatePrices(Station station) {
+    var dataChanged = false;
+    if (_petrol95Controller.text != '') {
+      station.price95 = _petrol95Controller.text;
+      _petrol95Controller.text = '';
+      dataChanged = true;
+    }
+    if (_petrol98Controller.text != '') {
+      station.price98 = _petrol98Controller.text;
+      _petrol98Controller.text = '';
+      dataChanged = true;
+    }
+    if (_petrolONController.text != '') {
+      station.priceON = _petrolONController.text;
+      _petrolONController.text = '';
+      dataChanged = true;
+    }
+    if (_petrolLPGController.text != '') {
+      station.priceLPG = _petrolLPGController.text;
+      _petrolLPGController.text = '';
+      dataChanged = true;
+    }
+    if (dataChanged) {
+      _db.addStation(station);
+    }
   }
 }
