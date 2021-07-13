73c5ce4b48b0d02cdce334576f0fa9f838f55e93
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-13 13:43:01 +0200

    Added id, rating and prices for station object

--- lib/Screens/Petrol_map/locations.g.dart
+++ lib/Screens/Petrol_map/locations.g.dart
@@ -8,28 +8,40 @@ part of 'locations.dart';
 
 Locations _$LocationsFromJson(Map<String, dynamic> json) {
   return Locations(
-    results: (json['results'] as List<dynamic>)
-        .map((e) => Result.fromJson(e as Map<String, dynamic>))
+    stations: (json['results'] as List<dynamic>)
+        .map((e) => Station.fromJson(e as Map<String, dynamic>))
         .toList(),
   );
 }
 
 Map<String, dynamic> _$LocationsToJson(Locations instance) => <String, dynamic>{
-      'results': instance.results,
+      'results': instance.stations,
     };
 
-Result _$ResultFromJson(Map<String, dynamic> json) {
-  return Result(
+Station _$ResultFromJson(Map<String, dynamic> json) {
+  return Station(
     geometry: Geometry.fromJson(json['geometry'] as Map<String, dynamic>),
     name: json['name'] as String,
+    rating: (json['rating'] == null ? 0 : json['rating'] as num).toDouble(),
     vicinity: json['vicinity'] as String,
+    place_id: json['place_id'] as String,
+    price95: 'b.d.',
+    price98: 'b.d.',
+    priceON: 'b.d.',
+    priceLPG: 'b.d.',
   );
 }
 
-Map<String, dynamic> _$ResultToJson(Result instance) => <String, dynamic>{
+Map<String, dynamic> _$ResultToJson(Station instance) => <String, dynamic>{
       'geometry': instance.geometry,
       'name': instance.name,
+      'rating': instance.rating,
       'vicinity': instance.vicinity,
+      'place_id': instance.place_id,
+      'price95': instance.price95,
+      'price96': instance.price98,
+      'priceON': instance.priceON,
+      'priceLPG': instance.priceLPG,
     };
 
 Geometry _$GeometryFromJson(Map<String, dynamic> json) {
