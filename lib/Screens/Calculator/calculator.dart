d85f2b352c426277d05026c3b4f94ffceb2831ab
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-14 11:53:32 +0200

    Added cost per 100km and code cleanup

--- lib/Screens/Calculator/calculator.dart
+++ lib/Screens/Calculator/calculator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fuel_tracker/services/authentication_services/auth_services.dart';
+import 'package:provider/provider.dart';
 
 class Calculator extends StatefulWidget {
   @override
@@ -12,6 +14,7 @@ class _MyAppState extends State<Calculator> {
   late TextEditingController _consumptionController;
   bool showResult = false;
   double totalCost = 0;
+  double hundredCost = 0;
 
   @override
   void initState() {
@@ -31,14 +34,13 @@ class _MyAppState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    final loginProvider = Provider.of<AuthServices>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Kalkulator kosztów'),
-        /*actions: [
-            IconButton(
-                onPressed: () async => await loginProvider.logout(),
-                icon: Icon(Icons.exit_to_app)),
-          ],*/
+        actions: [
+          IconButton(onPressed: () async => await loginProvider.logout(), icon: Icon(Icons.exit_to_app)),
+        ],
         flexibleSpace: Container(
           decoration: BoxDecoration(gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment(0.0, 2), colors: <Color>[Colors.green, Colors.lightGreen])),
         ),
@@ -52,165 +54,135 @@ class _MyAppState extends State<Calculator> {
                 key: _formkey,
                 child: Column(
                   children: <Widget>[
-                    SizedBox(
-                      height: 50,
-                    ),
-                    SizedBox(
-                      height: 60,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Odległość', style: TextStyle(fontSize: 17)),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Spacer(),
-                          Container(
-                            width: 200,
-                            child: TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _distanceController,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.amberAccent,
-                                labelText: '(km)',
-                                prefixIcon: Icon(Icons.add_road),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: const BorderSide(color: Colors.lightGreen, width: 3.0),
-                                ),
-                                border: const OutlineInputBorder(),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    SizedBox(height: 30),
+                    displayInputForm('Odległość', _distanceController, '(km)', Icon(Icons.add_road)),
                     SizedBox(height: 15),
-                    SizedBox(
-                      height: 60,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Cena', style: TextStyle(fontSize: 17)),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Spacer(),
-                          Container(
-                            width: 200,
-                            child: TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _priceController,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.amberAccent,
-                                labelText: '(zł/l)',
-                                prefixIcon: Icon(Icons.attach_money),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: const BorderSide(color: Colors.lightGreen, width: 3.0),
-                                ),
-                                border: const OutlineInputBorder(),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    displayInputForm('Cena', _priceController, '(zł/l)', Icon(Icons.attach_money)),
                     SizedBox(height: 15),
-                    SizedBox(
-                      height: 60,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Średnie spalanie', style: TextStyle(fontSize: 17)),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Spacer(),
-                          Container(
-                            width: 200,
-                            child: TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _consumptionController,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.amberAccent,
-                                labelText: '(l/100 km)',
-                                prefixIcon: Padding(padding: const EdgeInsets.all(14), child: Image.asset('assets/drop.png')),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: const BorderSide(color: Colors.lightGreen, width: 3.0),
-                                ),
-                                border: const OutlineInputBorder(),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 50),
-                    Center(
-                      child: MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            totalCost = (double.parse(_distanceController.text) / 100) * double.parse(_consumptionController.text) * double.parse(_priceController.text);
-                            showResult = true;
-                          });
-                        },
-                        height: 50,
-                        color: Colors.lightGreen,
-                        textColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Text(
-                          'Oblicz koszt',
-                          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
+                    displayInputForm('Średnie spalanie', _consumptionController, '(l/100km)', Padding(padding: const EdgeInsets.all(14), child: Image.asset('assets/drop.png'))),
+                    SizedBox(height: 30),
+                    displayApplyButton(),
                     SizedBox(height: 40),
-                    showResult == true
-                        ? Center(
-                            child: Column(children: [
-                              Text(
-                                'Obliczony koszt przejazdu',
-                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 20),
-                              Container(
-                                width: 200,
-                                decoration: BoxDecoration(
-                                  color: Colors.amber,
-                                  border: Border.all(
-                                    color: Colors.lightGreen,
-                                    width: 3.0,
-                                  ),
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(15),
-                                  ),
-                                ),
-                                child: Center(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      totalCost.toStringAsFixed(2) + ' zł',
-                                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ]),
-                          )
-                        : Container(),
+                    showResult == true ? displayCalculatedResult('Całkowity koszt przejazdu', totalCost.toStringAsFixed(2) + ' zł') : Container(),
+                    SizedBox(height: 20),
+                    showResult == true ? displayCalculatedResult('Koszt przejazdu 100km', hundredCost.toStringAsFixed(2) + ' zł') : Container(),
                   ],
                 ),
               ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget displayCalculatedResult(String resultTitle, String result) {
+    return Center(
+      child: Column(children: [
+        Text(
+          resultTitle,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 15),
+        Container(
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            border: Border.all(
+              color: Colors.lightGreen,
+              width: 3.0,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
+            ),
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                result,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget displayApplyButton() {
+    return Center(
+      child: MaterialButton(
+        onPressed: () async {
+          if (_formkey.currentState!.validate()) {
+            setState(
+              () {
+                try {
+                  totalCost = (double.parse(_distanceController.text) / 100) * double.parse(_consumptionController.text) * double.parse(_priceController.text);
+                  hundredCost = double.parse(_consumptionController.text) * double.parse(_priceController.text);
+                  showResult = true;
+                } catch (e) {
+                  print('Invalid format');
+                }
+                FocusScope.of(context).unfocus();
+              },
+            );
+          }
+        },
+        height: 50,
+        color: Colors.lightGreen,
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Text(
+          'Oblicz koszt',
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget displayInputForm(String inputName, TextEditingController inputController, String labelText, Widget? icon) {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(inputName, style: TextStyle(fontSize: 17)),
+          SizedBox(
+            width: 15,
+          ),
+          Spacer(),
+          Container(
+            width: 200,
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              controller: inputController,
+              validator: (val) => val!.isNotEmpty ? null : 'Podaj poprawną wartość',
+              decoration: InputDecoration(
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(color: Colors.green, width: 3.0),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(color: Colors.blueAccent, width: 3.0),
+                ),
+                errorStyle: TextStyle(color: Colors.lightGreen, fontSize: 15),
+                filled: true,
+                fillColor: Colors.amberAccent,
+                labelText: labelText,
+                prefixIcon: icon,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(color: Colors.lightGreen, width: 3.0),
+                ),
+                border: const OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
