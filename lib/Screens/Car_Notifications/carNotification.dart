c96bc8920a98b426f58954d39b010193ac776630
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-29 14:06:12 +0200

    Changed car notification class

--- lib/Screens/Car_Notifications/carNotification.dart
+++ lib/Screens/Car_Notifications/carNotification.dart
@@ -1,32 +1,40 @@
 class CarNotification {
   CarNotification({
+    required this.id,
     required this.title,
     required this.description,
     required this.date,
     required this.userID,
+    required this.isSet,
   });
 
   factory CarNotification.fromJson(Map<String, dynamic> json) {
-    return CarNotification(title: json['title'], description: json['description'], date: json['date'], userID: json['userID']);
+    return CarNotification(id: json['id'], title: json['title'], description: json['description'], date: json['date'], userID: json['userID'], isSet: json['isSet']);
   }
 
   CarNotification.fromMap(Map snapshot)
-      : title = snapshot['title'],
+      : id = snapshot['id'],
+        title = snapshot['title'],
         description = snapshot['description'],
         date = snapshot['date'].toDate(),
-        userID = snapshot['userID'];
+        userID = snapshot['userID'],
+        isSet = snapshot['isSet'];
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'title': title,
       'description': description,
       'date': date,
       'userID': userID,
+      'isSet': isSet,
     };
   }
 
+  int id;
   String title;
   String description;
   DateTime date;
   String userID;
+  bool isSet;
 }
