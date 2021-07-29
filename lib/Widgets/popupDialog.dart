67689c2c30057c6b255a7de8da4b5ae9f3e9fe36
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-29 13:06:38 +0200

    Added popup dialog to external widget

---
+++ lib/Widgets/popupDialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PopupDialog extends StatelessWidget {
+  PopupDialog({required this.title, required this.message, required this.close});
+
+  final String title;
+  final String message;
+  final String close;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      title: Text(title),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(message),
+        ],
+      ),
+      actions: <Widget>[
+        MaterialButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: Text(close),
+        ),
+      ],
+    );
+  }
+}
