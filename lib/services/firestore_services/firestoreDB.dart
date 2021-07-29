e90d9dc49a1f9c4beb2c74c0b002af9a011d3e57
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-07-29 12:15:45 +0200

    Added db function to add and get notifications

--- lib/services/firestore_services/firestoreDB.dart
+++ lib/services/firestore_services/firestoreDB.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:fuel_tracker/Screens/Car_Notifications/carNotification.dart';
 import 'package:fuel_tracker/Screens/Petrol_map/locations.dart';
 
 class FirestoreDB extends ChangeNotifier {
   final CollectionReference _stationsCollectionReference = FirebaseFirestore.instance.collection('stations');
+  final CollectionReference _notificationsCollectionReference = FirebaseFirestore.instance.collection('users');
 
   Future addStation(Station station) async {
     try {
@@ -19,6 +21,27 @@ class FirestoreDB extends ChangeNotifier {
   Future getStation(Station station) async {
     try {
       await _stationsCollectionReference.doc(station.place_id).get().then((snapshot) => station.setData(snapshot));
+    } catch (e) {
+      print(e.toString());
+      if (e is PlatformException) return e.message;
+      return e.toString();
+    }
+  }
+
+  Future addNotification(CarNotification notification) async {
+    try {
+      return await _notificationsCollectionReference.doc('notifications').collection(notification.userID).doc(notification.title).set(notification.toMap());
+    } catch (e) {
+      print(e.toString());
+      if (e is PlatformException) return e.message;
+      return e.toString();
+    }
+  }
+
+  Future getNotifications(String userID) async {
+    var notif = await _notificationsCollectionReference.doc('notifications').collection(userID).get();
+    try {
+      return notif.docs.map((snapshot) => CarNotification.fromMap(snapshot.data())).toList();
     } catch (e) {
       print(e.toString());
       if (e is PlatformException) return e.message;
